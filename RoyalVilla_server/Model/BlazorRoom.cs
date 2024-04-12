4ec9009df8ff90e68069a1bc59cb1d46664a0897
yudiz - devang <devang.amrutiya@yudiz.com>
2024-04-12 17:36:08 +0530

    Add project files.

---
+++ RoyalVilla_server/Model/BlazorRoom.cs
@@ -0,0 +1,13 @@
+﻿namespace RoyalVilla_server.Model
+{
+    public class BlazorRoom
+    {
+        public int Id { get; set; }
+
+        public string Roomname { get; set; }
+
+        public double Price { get; set; }   
+
+        public bool IsActive { get; set; }  
+    }
+}
